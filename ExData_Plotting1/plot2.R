afd02dd6b2b2436249f7bf6e0a3affb5b268ddfe
Eduvar <edvarela@hotmail.com>
2015-08-03 19:38:42 -0500

    Create plot2.R

---
+++ ExData_Plotting1/plot2.R
@@ -0,0 +1,128 @@
+Skip to content
+This repository  
+Pull requests
+Issues
+Gist
+ @Eduvar
+ Watch 1
+  Star 0
+  Fork 27,939
+EPadronU/ExData_Plotting1
+forked from rdpeng/ExData_Plotting1
+Branch: master  ExData_Plotting1/plot2.R
+@EPadronUEPadronU 25 days ago Clean some unused code from the scripts
+1 contributor
+RawBlameHistory     112 lines (92 sloc)  3.635 kB
+################################################################################
+################################################################################
+
+
+# Functions ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Download the raw data sets to use in this assignment.
+#
+# This function is aware of the OS in order to set the correct download method.
+#
+# @param dataSetsURL  character(1) The URL of the data sets.
+# @param dataSetsPath character(1) The file system path to use in order to save
+#                                  the data sets on disk.
+downloadDataSets <- function(dataSetsURL, dataSetsPath) {
+  downloadMethod <- if (Sys.info()['sysname'] == 'Windows') {
+    'auto'
+  } else {
+    'curl'
+  }
+
+  download.file(dataSetsURL, dataSetsPath, downloadMethod)
+}
+
+
+# Read a data set located inside the given zip file.
+#
+# @param zipFilePath    character(1) The path to the zip file.
+# @param zipDataSetPath character(1) The path to the data set inside the zip
+#                                    file.
+# @param ... Parameters to be passed to the `read.table` function
+#
+# @return data.frame A data frame with the indicated data set.
+readZippedDataSet <- function(zipFilePath, zipDataSetPath, ...) {
+  read.table(unz(zipFilePath, zipDataSetPath), ...)
+}
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+# Main ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Paths & URLs +++++++++++++++++++++++++++++++++++++++++++++
+dataDirPath <- file.path('.', 'data')
+dataSetsPath <- file.path('.', dataDirPath, 'datasets.zip')
+dataSetsURL <- 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# Download the data sets to disk if it's necessary +++++++++
+if (!file.exists(dataSetsPath)) {
+  if (!file.exists(dataDirPath)) {
+    dir.create(dataDirPath)
+  }
+
+  print('[INFO] Downloading the raw data sets...')
+
+  downloadDataSets(dataSetsURL, dataSetsPath)
+}
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# Load the data sets into memory +++++++++++++++++++++++++++
+print('[INFO] Loading the raw data sets into memory...')
+
+electricPowerConsumption <- readZippedDataSet(
+  dataSetsPath,
+  'household_power_consumption.txt',
+  col.names=c(
+    'Date',
+    'Time',
+    'Global_active_power',
+    'Global_reactive_power',
+    'Voltage',
+    'Global_intensity',
+    'Sub_metering_1',
+    'Sub_metering_2',
+    'Sub_metering_3'
+  ),
+  sep=';',
+  skip=66637,
+  nrows=2880
+)
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# Transform the Date and Time variables into a single one ++
+print('[INFO] Tidying the raw data sets...')
+
+dateTimeVector <- mapply(function(strDate, strTime) {
+  as.POSIXct(strptime(paste(' ', strDate, strTime), '%e/%m/%Y %H:%M:%S'))
+}, electricPowerConsumption$Date, electricPowerConsumption$Time)
+
+electricPowerConsumption <- data.frame(
+  DateTime=as.POSIXct(dateTimeVector, origin='1970-01-01'),
+  electricPowerConsumption[, 3:ncol(electricPowerConsumption)]
+)
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# Build the graph ++++++++++++++++++++++++++++++++++++++++++
+print('[INFO] Building the graph...')
+
+# Open a new file device
+png(filename='plot2.png')
+
+with(electricPowerConsumption, plot(
+  DateTime,
+  Global_active_power,
+  main='',
+  type='l',
+  xlab='',
+  ylab='Global Active Power (kilowatts)'
+))
+
+# Close the file device
+dev.off()
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Status API Training Shop Blog About Help
+© 2015 GitHub, Inc. Terms Privacy Security Contact
